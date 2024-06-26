0a8426b188d1d4453f270efa7b0e5ac6f86737a7
MatheshDev <matheshkumar010@gmail.com>
2024-06-26 21:28:04 +0530

    edited largeNum.py

--- largeNum.py
+++ largeNum.py
@@ -1,13 +1,10 @@
-import math
-
 numList = [2,6,8,5,3]
 
 def LargeNum(numList):
     large = numList[0]
     for num in numList:
-        if num >large:
+        if num > large:
             large = num
     return large
 
 print(LargeNum(numList))
-
